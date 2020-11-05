8f9342beeb61f56b863239c2997ed550c3c487dd
juiceghost <jg@renegade.se>
2020-11-05 12:22:19 +0100

    Made some functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wordList = ['Krille', 'Javascript', 'Kaffe', 'Te', 'Vatten']; // Array: med spelets alla ord
+const wordList = ['VS Code', 'Krille', 'Javascript', 'Kaffe', 'Te', 'Vatten']; // Array: med spelets alla ord
 
 // Funktion som slumpar fram ett ord
 
@@ -10,11 +10,34 @@ const wordList = ['Krille', 'Javascript', 'Kaffe', 'Te', 'Vatten']; // Array: me
 // OBS! Det måste vara olika ord varje gång (eller i alla slumpmässigt utvalt)
 
 function randomWord (arr) {
-    let randomNumber = Math.floor(Math.random() * arr.length)
-    console.log("Arrayen: " + arr)
-    console.log("randomNumber: " + randomNumber)
-    return  
+    const randomNumber = Math.floor(Math.random() * arr.length)
+    return arr[randomNumber]
 }
+//console.log("Function returns " + randomWord(wordList));
 
-randomWord(wordList);
+// Funktion som inaktiverar/aktiverar bokstavsknapparna (beroende på vilken del av spelet du är på)
 
+function activate (arg) {
+    //let myButtonContainer = document.querySelector('#letterButtons')
+    let myButtons = document.querySelectorAll('#letterButtons button')
+    // om arg = true, aktivera alla knappar
+    // (arg = true):
+    // 1. för varje knapp i listan:
+    // sätt den aktiv
+    for (let i = 0; i < myButtons.length; i++) {
+        console.log("index: " + i)
+        //console.log("element: ");
+        console.log(myButtons[i]) 
+        myButtons[i].disabled = false
+
+    }
+
+    // om arg = false, avaktivera --"--
+    // element.disabled = true avaktiverar, = false aktiverar
+    // myButtons.disabled = false funkar icke.
+
+    
+    return myButtons
+}
+console.log("Function returns ");
+console.log(activate());
